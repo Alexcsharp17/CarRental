95ccbb221a1551069d74e272a26c35c8730dd47b
Alexcsharp17 <55028956+Alexcsharp17@users.noreply.github.com>
2020-02-25 09:32:18 +0200

    Created project

---
+++ CarRental/CarRental.DAL/Entities/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.DAL.Entities
+{
+   public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public int Capacity { get; set; }
+        //(Four, front,rear)
+        public string DriveUnit { get; set; }
+        //(True-transsmision is automatic, False-manual)
+        public byte AutomaticTransm { get; set; }
+        public byte AirConditon{ get; set; }
+       
+        //The price is set in gryvnas per day.
+        public decimal Price { get; set; }
+        public virtual ICollection<CarType> CarTypes { get; set; }
+
+        public Car()
+        {
+            CarTypes = new List<CarType>();
+        }
+    }
+}
---
+++ CarRental/CarRental.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarRental.WEB.Startup))]
+namespace CarRental.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
